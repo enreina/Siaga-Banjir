ea40e2cdfeaeae39645eee5e821cb5c66610275f
reyreina <enreina@gmail.com>
2014-05-03 08:25:27 +0700

    integrate with UI

---
+++ src/com/siagabanjir/HomeFragment.java
@@ -0,0 +1,190 @@
+package com.siagabanjir;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.siagabanjir.utility.JSONParser;
+
+import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.Toast;
+
+@SuppressLint("ValidFragment")
+public class HomeFragment extends ListFragment {
+	private ArrayList<DataPintuAir> pintuAir;
+	BinderData binder;
+	private boolean empty;
+	
+	private Context context;
+	public HomeFragment() {
+		this(0);
+	}
+	
+	public HomeFragment(ArrayList<DataPintuAir> pintuAir, Context context) {
+		this.pintuAir = pintuAir;
+		this.context = context;
+		refreshHome();
+	}
+		
+	public HomeFragment(int status) {
+		pintuAir = new ArrayList<DataPintuAir>();
+
+		refreshHome();
+	}
+	
+	public void refreshHome() {
+		new JSONParse().execute();
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		binder = new BinderData(getActivity(), pintuAir);
+		setListAdapter(binder);
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.list_fragment, container, false);
+	}
+
+	/*
+	@Override
+	public void onListItemClick(ListView list, View v, int position, long id) {
+		if (this.empty == false) {
+			Intent i = new Intent(this.getActivity().getBaseContext(), DetailActivity.class);
+			
+			i.putParcelableArrayListExtra("pintuair", pintuAir);
+			i.putExtra("selected", position);
+			
+			startActivity(i);
+		} 
+		
+	}*/
+	
+	public void addData(DataPintuAir dp) {
+		pintuAir.add(dp);
+	}
+
+	public void clearData() {
+		// TODO Auto-generated method stub
+		pintuAir.clear();
+	}
+	
+	public void refresh() {
+		binder.notifyDataSetChanged();
+		if (binder.isEmpty()) {
+			this.getListView().setVisibility(View.GONE);
+			this.getActivity().findViewById(R.id.tvEmpty).setVisibility(View.VISIBLE);
+		} else {
+			this.getListView().setVisibility(View.VISIBLE);
+			this.getActivity().findViewById(R.id.tvEmpty).setVisibility(View.GONE);
+		}
+	}
+	
+
+	private class JSONParse extends AsyncTask<String, String, JSONObject> {
+		private static final String url = "http://labs.pandagostudio.com/siaga-banjir/";
+		ProgressDialog pd;
+		protected void onPreExecute() {
+			Toast.makeText(context, "Updating data...", Toast.LENGTH_LONG).show();
+			pd = new ProgressDialog(context);
+			pd.setTitle("Updating data...");
+			pd.setMessage("Please wait.");
+			pd.setCancelable(false);
+			pd.setIndeterminate(true);
+			pd.show();
+		}
+		@Override
+		protected JSONObject doInBackground(String... params) {
+			// TODO Auto-generated method stub
+			JSONObject json = null;
+			try {
+				JSONParser jParser = new JSONParser();
+				json = jParser.getJSONFromUrl(url);
+				Thread.sleep(5000);
+			} catch(InterruptedException e) {
+				
+			}
+			return json;
+		}
+		
+		protected void onProgressUpdate(String... values) {
+			Toast.makeText(context, "Updating data...", Toast.LENGTH_LONG).show();
+		}
+		
+		
+		protected void onPostExecute(JSONObject json) {
+			pintuAir.clear();
+			
+			JSONArray dataPintuAir;
+			try {
+				dataPintuAir = json.getJSONArray("datapintuair");
+				for (int ii=0; ii<dataPintuAir.length(); ii++) {
+					JSONObject obj = dataPintuAir.getJSONObject(ii);
+					String nama = obj.getString("nama");
+					String tanggal = obj.getString("tanggal");
+					JSONArray dataTinggi = obj.getJSONArray("tinggiair");
+					
+					DataPintuAir dp = new DataPintuAir(nama);
+					dp.setTanggal(tanggal);
+					
+					for (int jj=0; jj<dataTinggi.length(); jj++) {
+						int tinggi = 0;
+						String status = dataTinggi.getJSONObject(jj).getString("status");
+						if (!dataTinggi.getJSONObject(jj).getString("tinggi").split(" ")[0].equals("-")) {
+							tinggi = Integer.parseInt(dataTinggi.getJSONObject(jj).getString("tinggi").split(" ")[0]);
+						} else {
+							status = "N/A";
+						}
+						int waktu = dataTinggi.getJSONObject(jj).getInt("waktu");
+						dp.addTinggiAir(tinggi, status, waktu);
+					}
+					
+					String status = dp.getStatus()[0];
+					
+					
+					pintuAir.add(dp);
+					
+				}
+				Collections.sort(pintuAir);
+				refresh();
+				
+				Toast.makeText(context, "Done!", Toast.LENGTH_LONG).show();
+				if (pd != null) {
+					pd.dismiss();
+				}
+
+				
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				Toast.makeText(context, "No internet connection", Toast.LENGTH_LONG).show();
+				if (pd != null) {
+					pd.dismiss();
+				}
+			} catch (NullPointerException e) {
+				Toast.makeText(context, "Error fetching data or no internet connection", Toast.LENGTH_LONG).show();
+				if (pd != null) {
+					pd.dismiss();
+				}
+			}
+			
+			
+		}
+		
+	}
+}
---
+++ src/com/siagabanjir/TestActivity.java
@@ -0,0 +1,218 @@
+package com.siagabanjir;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnMapClickListener;
+import com.google.android.gms.maps.GoogleMap.OnMapLongClickListener;
+import com.google.android.gms.maps.GoogleMap.OnMarkerDragListener;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.MarkerOptionsCreator;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
+import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.widget.Toast;
+
+public class TestActivity extends FragmentActivity implements OnMapClickListener, OnMapLongClickListener, OnMarkerDragListener, ConnectionCallbacks, OnConnectionFailedListener, LocationListener {
+	private Context context;
+	private Fragment fragment;
+	
+	private ActionBar actionBar;
+	
+	private LocationClient locationClient;
+	private LocationRequest locationRequest;
+	private boolean locationEnabled;
+	
+	
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		context = this;
+		
+		
+		fragment = new MyPlaceFragment(context);
+		FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+		ft.add(R.id.container, fragment,"MY_PLACE_FRAGMENT").commit();
+		
+
+		setContentView(R.layout.activity_home);
+		
+		setupUserLocation();
+		
+		
+	}
+	
+	private void setupUserLocation() {
+		locationClient = new LocationClient(this, this, this);
+		locationRequest = new LocationRequest();
+		
+		locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+		locationRequest.setInterval(5);
+		locationRequest.setFastestInterval(1);
+		
+		LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+		if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) && !locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
+			locationEnabled = false;
+			Toast.makeText(this, "Enable location services for accurate data", Toast.LENGTH_SHORT).show();
+		}
+		
+		else  {
+			locationEnabled = true;
+		}
+		
+		locationClient.connect();
+		
+	}
+
+	@Override
+	public void onConnectionFailed(ConnectionResult arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onConnected(Bundle arg0) {
+		// TODO Auto-generated method stub
+		//locationClient.setMockMode(true);
+		Location location = locationClient.getLastLocation();
+		if (location != null) {
+			Geocoder geocoder = new Geocoder(this);
+			try {
+				List<Address> address = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+				address.get(0);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			Toast.makeText(this, "Initial location: " + location.getLatitude() + ", " + location.getLongitude(), Toast.LENGTH_LONG).show();
+			LatLng currentLoc = new LatLng(location.getLatitude(), location.getLongitude());
+			refreshMap(currentLoc);
+			
+		}
+		else if (location == null && locationEnabled) {
+			locationClient.requestLocationUpdates(locationRequest, this);
+		} 
+	}
+	
+	public void refreshMap(LatLng currentLoc) {
+		DataPintuAir.initLocation();
+		
+		GoogleMap peta = ((MyPlaceFragment) getSupportFragmentManager().findFragmentById(R.id.container)).peta;
+		peta.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLoc, 15));
+		
+		MarkerOptions marker = new MarkerOptions();
+		marker.position(currentLoc);
+		Address addr;
+		try {
+			addr = new Geocoder(this).getFromLocation(marker.getPosition().latitude, marker.getPosition().longitude, 1).get(0);
+			System.out.println();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		marker.draggable(true);
+		marker.icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_location));
+		
+		peta.setOnMarkerDragListener(this);
+		peta.addMarker(marker);
+		
+		for (LatLng loc : DataPintuAir.locationPintuAir.values()) {
+			MarkerOptions markerPintuAir = new MarkerOptions().position(loc);
+			
+			peta.addMarker(markerPintuAir);
+			CircleOptions circle = new CircleOptions();
+			
+			int strokeColor = 0xffff0000; //red outline
+		    int shadeColor = 0x44ff0000;
+		    
+			circle.center(loc);
+			circle.radius(4000.0f);
+			circle.strokeColor(strokeColor);
+			circle.fillColor(shadeColor);
+			
+			peta.addCircle(circle);
+		}
+		
+	}
+
+	@Override
+	public void onDisconnected() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onLocationChanged(Location location) {
+		locationClient.removeLocationUpdates(this);
+		Toast.makeText(this, "Location: " + location.getLatitude() + ", " + location.getLongitude(), Toast.LENGTH_SHORT).show();
+		
+		
+	}
+
+	@Override
+	public void onMapLongClick(LatLng arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onMapClick(LatLng arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onMarkerDrag(Marker arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onMarkerDragEnd(Marker marker) {
+		checkLocation(marker);
+		
+	}
+	
+	public void checkLocation(Marker marker) {
+		HashMap<String, LatLng> inArea = DataPintuAir.checkLocation(marker.getPosition());
+		
+		
+		String pintuAir = "";
+		for (String locName : inArea.keySet()) {
+			pintuAir += locName + ", ";
+		}
+		
+		
+		Toast.makeText(this, "Selected location: " + marker.getPosition().latitude + ", " + marker.getPosition().longitude + "\nNearest floodgates: " + pintuAir, Toast.LENGTH_LONG).show();
+		
+	}
+
+	@Override
+	public void onMarkerDragStart(Marker arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+}
