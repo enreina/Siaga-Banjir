82b166be6909a332414afc92e64a1e6f2aeee2f5
reyreina <enreina@gmail.com>
2014-05-11 23:45:02 +0700

    english version final

--- src/com/siagabanjir/MainActivity.java
+++ src/com/siagabanjir/MainActivity.java
@@ -71,7 +71,7 @@ public class MainActivity extends ActionBarActivity implements
 			*/
 
 			ActionBar actionBar = getSupportActionBar();
-			actionBar.setIcon(R.drawable.ico_actionbar);
+			actionBar.setIcon(R.drawable.ico_actionbarcopy);
 			actionBar.setDisplayShowTitleEnabled(false);
 
 			// setUp data
